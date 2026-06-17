ac5b8ef43fc06f44f1bd7b7aa4f25e904a431a1f
Manuel Adolfo Fernández <maff@Mac-mini-de-Manuel-Adolfo-Fernandez.local>
2026-06-17 11:15:05 +0200

    style: remove background from logo assets and remove clip-path to show full silhouette

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -594,7 +594,7 @@ export default function Sidebar({ isOpen, onClose }: { isOpen?: boolean; onClose
             <img
               src="/logo-familiar-transparente.png"
               alt="Álbum Familiar"
-              className="w-30 sm:w-34 h-auto object-contain bg-transparent [filter:url(#logo-modo-claro)] dark:[filter:url(#logo-modo-oscuro)] [clip-path:inset(7%)] select-none"
+              className="w-30 sm:w-34 h-auto object-contain bg-transparent [filter:url(#logo-modo-claro)] dark:[filter:url(#logo-modo-oscuro)] select-none"
             />
 
             {/* Renderizado de corazones flotantes */}
